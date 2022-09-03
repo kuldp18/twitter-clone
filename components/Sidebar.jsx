02db9678e65a4d62bc2bbf8a1498b7164fd76d7c
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-09-03 17:44:06 +0530

    sidebar complete

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -10,6 +10,7 @@ import {
   EllipsisHorizontalCircleIcon,
   UserIcon,
   ListBulletIcon,
+  EllipsisHorizontalIcon,
 } from '@heroicons/react/24/outline';
 
 const Sidebar = () => {
@@ -28,6 +29,25 @@ const Sidebar = () => {
         <SidebarLink text="Lists" Icon={ListBulletIcon} />
         <SidebarLink text="Profile" Icon={UserIcon} />
         <SidebarLink text="More" Icon={EllipsisHorizontalCircleIcon} />
+      </div>
+
+      <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cda]">
+        Tweet
+      </button>
+
+      <div className="text-[#d9d9d9] flex items-center justify-center mt-auto hoverAnimation xl:ml-auto xl:-mr-5">
+        <img
+          src="https://pbs.twimg.com/profile_images/1539218328270835712/IpqTJMJj_400x400.jpg"
+          alt=""
+          className="h-10 w-10 rounded-full xl:mr-2.5"
+        />
+
+        <div className="hidden xl:inline leading-5">
+          <h4 className="font-bold">firebase 1875</h4>
+          <p className="text-[#6e767d]">@firebase1875</p>
+        </div>
+
+        <EllipsisHorizontalIcon className="h-5 hidden xl:inline ml-10" />
       </div>
     </div>
   );
