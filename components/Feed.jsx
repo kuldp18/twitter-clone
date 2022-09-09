79a0aced30c1cf76dd61d8895c965c6bf3e9c155
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-09-09 22:33:15 +0530

    fetch posts from firestore and created post component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,26 @@
-import Input from './Input';
 import { SparklesIcon } from '@heroicons/react/24/outline';
+import { useEffect, useState } from 'react';
+import Input from './Input';
+import { onSnapshot, collection, query, orderBy } from '@firebase/firestore';
+import { db } from '../firebase';
+import Post from './Post';
+import { useSession } from 'next-auth/react';
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  // fetch latest posts
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
     <div className="text-white  flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
       <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
@@ -12,6 +31,12 @@ const Feed = () => {
       </div>
 
       <Input />
+
+      <div className="pb-72">
+        {posts.map((post) => (
+          <Post key={post.id} id={post.id} post={post.data()} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ components/Post.jsx
@@ -0,0 +1,55 @@
+const Post = ({ id, post, postPage }) => {
+  return (
+    <div className="p-3 flex cursor-pointer border-b border-gray-700">
+      {!postPage && (
+        <img
+          src={post?.userImg}
+          alt="Profile Pic"
+          className="h-11 w-11 rounded-full mr-4"
+        />
+      )}
+
+      <div className="flex flex-col space-y-2 w-full">
+        <div className={`flex ${!postPage && 'justify-between'}`}>
+          {postPage && (
+            <img
+              src={post?.userImg}
+              alt="Profile Pic"
+              className="h-11 w-11 rounded-full mr-4"
+            />
+          )}
+
+          <div className="text-[#6e767d]">
+            <div className="inline-block group">
+              <h4
+                className={`font-bold text-[15px] sm:text-base text-[#d9d9d9] group-hover:underline
+                ${!postPage && 'inline-block'}`}
+              >
+                {post?.username}
+              </h4>
+              <span
+                className={`text-sm sm:text-[15px] ${!postPage && 'ml-1.5'}`}
+              >
+                @{post?.tag}
+              </span>
+            </div>
+
+            <span className="hover:underline text-sm sm:text-[15px]">
+              {/* <Moment fromNow={post?.timestamp?.toDate()}></Moment> */}
+            </span>
+
+            {!postPage && (
+              <p className="text-[#d9d9d9] text-[15px] sm:text-base mt-0.5">
+                {post?.text}
+              </p>
+            )}
+          </div>
+          {/* 
+          <div>icon</div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
