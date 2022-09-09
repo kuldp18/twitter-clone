33132312a681a3cda1028e06693f99721f50ffd4
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-09-09 20:35:44 +0530

    added Login component and finished login function

---
+++ components/Login.jsx
@@ -0,0 +1,36 @@
+import { signIn } from 'next-auth/react';
+import Image from 'next/image';
+
+const Login = ({ providers }) => {
+  return (
+    <div className="flex flex-col items-center space-y-20 pt-48">
+      <Image
+        src="https://rb.gy/ogau5a"
+        width={150}
+        height={150}
+        objectFit="contain"
+      />
+
+      <div>
+        {Object.values(providers).map((provider) => {
+          return (
+            <div key={provider.name}>
+              <button
+                class="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
+                onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+              >
+                <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#1d9bf0] rounded-full group-hover:w-56 group-hover:h-56"></span>
+                <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+                <span class="relative font-bold">
+                  Signin with {provider.name}
+                </span>
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,14 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Feed from '../components/Feed';
 import Sidebar from '../components/Sidebar';
+import Login from '../components/Login';
 import { getProviders, getSession, useSession } from 'next-auth/react';
 
-export default function Home() {
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession();
+
+  if (!session) return <Login providers={providers} />;
+
   return (
     <div>
       <Head>
@@ -25,12 +30,15 @@ export default function Home() {
 }
 
 export async function getServerSideProps(context) {
-  const trendingResults = await fetch('https://jsonkeeper.com/b/NKEV').then(
-    (res) => res.json()
-  );
-  const followResults = await fetch('https://jsonkeeper.com/b/WWMJ').then(
-    (res) => res.json()
-  );
+  // const trendingResults = await fetch('https://jsonkeeper.com/b/NKEV')
+  //   .then((res) => res.json())
+  //   .catch((err) => console.log(err));
+  // const followResults = await fetch('https://jsonkeeper.com/b/WWMJ')
+  //   .then((res) => res.json())
+  //   .catch((err) => console.log(err));
+
+  const trendingResults = 'Treanding Results';
+  const followResults = 'Follow Results';
   const providers = await getProviders();
   const session = await getSession(context);
 
