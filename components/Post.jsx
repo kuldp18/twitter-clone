80276f12d77bc615bb2ccad2f49d109df39497c4
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-09-10 12:54:35 +0530

    added tweet icons and installed moment for time and recoil for state management

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,4 +1,42 @@
+import { useState } from 'react';
+import {
+  EllipsisHorizontalIcon,
+  ChatBubbleLeftIcon,
+  TrashIcon,
+  ArrowPathRoundedSquareIcon,
+  HeartIcon,
+  ShareIcon,
+  ChartBarIcon,
+} from '@heroicons/react/24/outline';
+import {
+  HeartIcon as HeartIconFilled,
+  ChatBubbleLeftIcon as ChatBubbleFilled,
+} from '@heroicons/react/24/solid';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+} from '@firebase/firestore';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import Moment from 'react-moment';
+import { useRecoilState } from 'recoil';
+import { modalState, postIdState } from '../atoms/modalAtom';
+import { db } from '../firebase';
+
 const Post = ({ id, post, postPage }) => {
+  const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
+  const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [liked, setLiked] = useState(false);
+  const router = useRouter();
+
   return (
     <div className="p-3 flex cursor-pointer border-b border-gray-700">
       {!postPage && (
@@ -44,8 +82,99 @@ const Post = ({ id, post, postPage }) => {
               </p>
             )}
           </div>
-          {/* 
-          <div>icon</div> */}
+
+          <div className="icon group flex-shrink-0 ml-auto">
+            <EllipsisHorizontalIcon className="h-5 text-[#6e767d] group-hover:text-[#1d9bf0]" />
+          </div>
+        </div>
+
+        {postPage && (
+          <p className="text-[#d9d9d9] text-[15px] sm:text-base mt-0.5">
+            {post?.text}
+          </p>
+        )}
+
+        <img
+          src={post?.image}
+          alt=""
+          className="rounded-2xl max-h-[600px] object-cover mr-2"
+        />
+
+        <div
+          className={`text-[#6e767d] flex justify-between w-10/12 ${
+            postPage && 'mx-auto'
+          }`}
+        >
+          <div
+            className="flex items-center space-x-1 group"
+            onClick={(e) => {
+              e.stopPropagation();
+              setPostId(id);
+              setIsOpen(true);
+            }}
+          >
+            <div className="icon group-hover:bg-[#1d9bf0] group-hover:bg-opacity-10">
+              <ChatBubbleLeftIcon className="h-5 group-hover:text-[#1d9bf0]" />
+            </div>
+            {comments.length > 0 && (
+              <span className="group-hover:text-[#1d9bf0] text-sm">
+                {comments.length}
+              </span>
+            )}
+          </div>
+
+          {session.user.uid === post?.id ? (
+            <div
+              className="flex items-center space-x-1 group"
+              onClick={(e) => {
+                e.stopPropagation();
+                deleteDoc(doc(db, 'posts', id));
+                router.push('/');
+              }}
+            >
+              <div className="icon group-hover:bg-red-600/10">
+                <TrashIcon className="h-5 group-hover:text-red-600" />
+              </div>
+            </div>
+          ) : (
+            <div className="flex items-center space-x-1 group">
+              <div className="icon group-hover:bg-green-500/10">
+                <ArrowPathRoundedSquareIcon className="h-5 group-hover:text-green-500" />
+              </div>
+            </div>
+          )}
+
+          <div
+            className="flex items-center space-x-1 group"
+            onClick={(e) => {
+              e.stopPropagation();
+              likePost();
+            }}
+          >
+            <div className="icon group-hover:bg-pink-600/10">
+              {liked ? (
+                <HeartIconFilled className="h-5 text-pink-600" />
+              ) : (
+                <HeartIcon className="h-5 group-hover:text-pink-600" />
+              )}
+            </div>
+            {likes.length > 0 && (
+              <span
+                className={`group-hover:text-pink-600 text-sm ${
+                  liked && 'text-pink-600'
+                }`}
+              >
+                {likes.length}
+              </span>
+            )}
+          </div>
+
+          <div className="icon group">
+            <ShareIcon className="h-5 group-hover:text-[#1d9bf0]" />
+          </div>
+          <div className="icon group">
+            <ChartBarIcon className="h-5 group-hover:text-[#1d9bf0]" />
+          </div>
         </div>
       </div>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 
 import { SessionProvider } from 'next-auth/react';
+import { RecoilRoot } from 'recoil';
 
 export default function App({
   Component,
@@ -8,7 +9,9 @@ export default function App({
 }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   );
 }
