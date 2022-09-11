2fac589be8c2b7c1d6f3b652bc59140a37458b48
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-09-11 20:57:56 +0530

    error fixes

--- components/Login.jsx
+++ components/Login.jsx
@@ -22,12 +22,12 @@ const Login = ({ providers }) => {
           return (
             <div key={provider.name}>
               <button
-                class="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
+                className="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
                 onClick={() => signIn(provider.id, { callbackUrl: '/' })}
               >
-                <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#1d9bf0] rounded-full group-hover:w-56 group-hover:h-56"></span>
-                <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
-                <span class="relative font-bold">
+                <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#1d9bf0] rounded-full group-hover:w-56 group-hover:h-56"></span>
+                <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+                <span className="relative font-bold">
                   Signin with {provider.name}
                 </span>
               </button>
